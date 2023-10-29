c29eac833cff6d3a100c32cedc1ad569c4c35467
Sergio Peterson <Sergiopeter2016@berkeley.edu>
2023-10-29 05:25:59 -0700

    Something

--- src/login.tsx
+++ src/login.tsx
@@ -10,14 +10,20 @@ const Login = () => {
   const [username, setUsername] = useState<string>('');
   // State to determine if UserDataDisplay should be shown
   const [showUserDataDisplay, setShowUserDataDisplay] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
+    setError(null); // Clear the error when user types
   }
 
   const performGetUserData = useAction(api.Api_call.getUserData);
 
   const saveUsername = () => {
+    if (!username.trim()) { // Check if username is empty or just spaces
+      setError("Please enter a username.");
+      return;
+    }
     console.log(username);
     const dataPromise = Promise.resolve(performGetUserData({username: username, offset: 0}));
     dataPromise.then((data) => {
@@ -44,6 +50,7 @@ const Login = () => {
           value={username}
           onChange={handleInputChange}
         />
+        {error && <div className="text-danger mb-2">{error}</div>}
         
         <button className="btn btn-primary mt-2" onClick={saveUsername}>Save Username</button>
       </div>
