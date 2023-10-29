f31b34c1773adbb46e3ed4a5ce820c8c47250d6a
Alex Jean <ajean757@gmail.com>
2023-10-29 08:14:45 -0700

    added anime api call

--- convex/Api_call.ts
+++ convex/Api_call.ts
@@ -31,6 +31,22 @@ export const getUserData = action({
     const data = await fetch(`https://api.myanimelist.net/v2/users/${args.username}/animelist?fields=list_status&limit=100&offset=${args.offset}`, {headers});
     const obj = await data.json()
     return obj;
-    // do something with data
   },
 });
+
+export const getAnimeData = action({
+  args: { id: v.string() },
+  handler: async (_, args) => {
+    console.log(`Querying for anime with id ${args.id}...`);
+    const apiKey = process.env.MAL_API_KEY!;
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-MAL-CLIENT-ID': apiKey,
+    }
+    console.log(apiKey);
+    const data = await fetch(`https://api.myanimelist.net/v2/anime/${args.id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics`, {headers});
+    const obj = await data.json()
+    return obj;
+  },
+});
+
